7f59276600e73d3375fbaad219b8ba7528cbf8e1
Joshua Rymer <joshrymer0@gmail.com>
2025-12-03 17:58:06 +0000

    add AdvertBanner widget for promotional display

---
+++ lib/Advert.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AdvertBanner extends StatelessWidget {
+  const AdvertBanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+      color: Colors.purple,
+      child: const Text(
+        'BIG SALE! OUR ESSENTIAL RANGE HAS DROPPED IN PRICE! OVER 20% OFF! COME GRAB YOURS WHILE STOCK LASTS!',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+}
