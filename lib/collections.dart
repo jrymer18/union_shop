dcb0cb5ebe49607e58debe8966fe67079fd256e9
Joshua Rymer <joshrymer0@gmail.com>
2025-12-03 17:24:01 +0000

    add CollectionsPage with basic layout and navigation

--- lib/collections.dart
+++ lib/collections.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
+import 'navbar.dart';
+import 'footer.dart';
 
+class CollectionsPage extends StatelessWidget {
+  const CollectionsPage({super.key});
+
+  void _onPlaceholderPressed() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      // HEADER
+      body: Column(
+        children: [
+          NavBar(onPlaceholderPressed: _onPlaceholderPressed),
+
+          // PAGE CONTENT
+          Expanded(
+            child: Center(
+              child: Text(
+                'Collections content goes here',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            ),
+          ),
+
+          // FOOTER
+          const AppFooter(),
+        ],
+      ),
+    );
+  }
+}
